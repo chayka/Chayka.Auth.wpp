9e45176991b4f676cc22b10096295302e072eb83
Boris Mossounov <borix@tut.by>
2014-10-31 16:10:50 +0300

    Adding NLS

--- app/nls/authForm._.php
+++ app/nls/authForm._.php
@@ -1,7 +1,32 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: borismossounov
- * Date: 30.10.14
- * Time: 18:05
- */ 
+
+return array(
+    'header_login_screen' => 'Вход на сайт',
+    'message_validate_email' => 'Неверный формат (например: %s)',
+    'label_email' => 'Адрес e-mail',
+    'label_password' => 'Пароль',
+    'header_join_screen' => 'Регистрация',
+    'label_username' => 'Ваш псевдоним',
+    'message_validate_username' => 'Допустимые символы: латиница, цифры, без пробелов',
+    'header_forgot_password_screen' => 'Восстановление пароля',
+    'message_forgot_password' => 'Введите e-mail адрес, указанный при регистрации, и мы вышлем на него код для смены пароля.',
+    'header_change_password_screen' => 'Смена пароля',
+    'label_old_pass' => 'Старый пароль',
+    'label_new_pass' => 'Новый пароль',
+    'label_once_again' => 'еще раз',
+    'header_logout_screen' => 'Выход с сайта',
+    'message_want_to_sign_out' => 'Вы уверены в том, что хотите покинуть сайт?',
+    'button_login' => 'Войти',
+    'button_join' => 'Зарегистрироваться',
+    'button_send_code' => 'Выслать код',
+    'button_password_reset' => 'Сменить пароль',
+    'button_password_change' => 'Сменить пароль',
+    'button_logout' => 'Выход',
+    'button_cancel' => 'Отмена',
+    'link_forgot_password' => 'Забыли пароль?',
+    'link_login' => 'Войти',
+    'link_join' => 'Зарегистрироваться',
+    'link_change_password' => 'Сменить пароль',
+    'message_required' => 'Необходимо заполнить',
+
+);
