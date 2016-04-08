e597e3a8374936cd630cd6fdc4a99a9a846a5983
Boris Mossounov <borix@tut.by>
2016-04-08 19:01:44 +0300

    minor fix

--- Chayka.Auth.wpp.php
+++ Chayka.Auth.wpp.php
@@ -11,15 +11,27 @@
 
 require_once 'vendor/autoload.php';
 
-if(!class_exists("Chayka\\WP\\Plugin")){
-    add_action( 'admin_notices', function () {
-?>
-    <div class="error">
-        <p>Chayka Framework functionality is not available</p>
-    </div>
-<?php
-	});
-}else{
+$requirements = [
+    "Chayka\\WP\\Plugin" => 'Chayka.Core plugin is required in order for Chayka.Auth to work properly',
+    "Chayka\\Email\\Plugin" => 'Chayka.Email plugin is required in order for Chayka.Auth to work properly',
+];
+
+$requirementsMet = true;
+
+foreach($requirements as $cls => $message){
+    if(!class_exists($cls)){
+        $requirementsMet &= false;
+        add_action( 'admin_notices', function () use ($message){
+            ?>
+            <div class="error">
+                <p><?php echo $message; ?></p>
+            </div>
+            <?php
+        });
+    }
+}
+
+if($requirementsMet){
     require_once dirname(__FILE__).'/Plugin.php';
 	add_action('init', array("Chayka\\Auth\\Plugin", "init"));
     require_once dirname(__FILE__).'/Sidebar.php';
